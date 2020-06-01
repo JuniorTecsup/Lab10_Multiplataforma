b1cedcfc5292e5f034f65e64342a0ea8fc82f1a9
hdelacruz-tecsup <hugo.delacruz@tecsup.edu.pe>
2020-06-01 17:57:06 -0500

    Version1

---
+++ Ejercicio07_Vilchez/Models/HomeMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio07_Vilchez.Models
+{
+    public enum MenuItemType
+    {
+        
+        ViewToViewDemo,
+        BindingModeDemo,
+        ListViewDemo,
+        PickerDemo
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ Ejercicio07_Vilchez/Views/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Ejercicio07_Vilchez.Models;
+
+namespace Ejercicio07_Vilchez.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
+    {
+        Dictionary<int, NavigationPage> MenuPages = new Dictionary<int, NavigationPage>();
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MasterBehavior = MasterBehavior.Popover;
+
+            //MenuPages.Add((int)MenuItemType.Browse, (NavigationPage)Detail);
+        }
+
+        public async Task NavigateFromMenu(int id)
+        {
+            if (!MenuPages.ContainsKey(id))
+            {
+                switch (id)
+                {
+                    
+                    
+                    case (int)MenuItemType.ViewToViewDemo:
+                        MenuPages.Add(id, new NavigationPage(new ViewToViewDemo()));
+                        break;
+                    case (int)MenuItemType.BindingModeDemo:
+                        MenuPages.Add(id, new NavigationPage(new BindingModeDemo()));
+                        break;
+                    case (int)MenuItemType.ListViewDemo:
+                        MenuPages.Add(id, new NavigationPage(new ListViewDemo()));
+                        break;
+                    case (int)MenuItemType.PickerDemo:
+                        MenuPages.Add(id, new NavigationPage(new PickerDemo()));
+                        break;
+                }
+            }
+
+            var newPage = MenuPages[id];
+
+            if (newPage != null && Detail != newPage)
+            {
+                Detail = newPage;
+
+                if (Device.RuntimePlatform == Device.Android)
+                    await Task.Delay(100);
+
+                IsPresented = false;
+            }
+        }
+    }
+}
